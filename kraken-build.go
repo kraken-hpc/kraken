029d896e874f549a088890e9cae9004c72bc96e8
J. Lowell Wofford <lowell@lanl.gov>
2018-12-14 12:35:12 -0700

    kraken-build: CGO_ENABLED=0; import environ; added force and verbose flags

--- kraken-build.go
+++ kraken-build.go
@@ -52,22 +52,29 @@ func compileTemplates(dir string) (e error) {
 	return
 }
 
-func buildKraken(dir string, t Target) (e error) {
+func buildKraken(dir string, t Target, verbose bool) (e error) {
 	// setup log file
 	var f *os.File
-	f, e = os.OpenFile(filepath.Join(dir, "log"), os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
-	if e != nil {
-		return e
+	if verbose {
+		f = os.Stderr
+	} else {
+		f, e = os.OpenFile(filepath.Join(dir, "log"), os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+		if e != nil {
+			return e
+		}
+		defer f.Close()
 	}
-	defer f.Close()
 
 	cmd := exec.Command("go", "build", "main.go")
 	cmd.Dir = dir
+
+	cmd.Env = os.Environ()
+	cmd.Env = append(cmd.Env, "CGO_ENABLED=0")
 	cmd.Env = append(cmd.Env, "GOOS="+t.Os)
 	cmd.Env = append(cmd.Env, "GOARCH="+t.Arch)
-
 	cmd.Env = append(cmd.Env, "GOPATH="+build.Default.GOPATH)
 	cmd.Env = append(cmd.Env, "GOROOT="+build.Default.GOROOT)
+
 	cmd.Stdout = f
 	cmd.Stderr = f
 	e = cmd.Run()
@@ -79,6 +86,8 @@ func main() {
 	cfgFile := flag.String("config", "config/kraken.yaml", "specify the build configuration YAML file")
 	buildDir := flag.String("dir", "build", "specify directory to put built binaries in")
 	noCleanup := flag.Bool("noclean", false, "don't cleanup temp dir after build")
+	force := flag.Bool("force", false, "force will overwrite existing build targets")
+	verbose := flag.Bool("v", false, "verbose will print extra information about the build process")
 	flag.Parse()
 
 	// read config
@@ -132,11 +141,19 @@ func main() {
 	// build
 	for t := range cfg.Targets {
 		log.Printf("building: %s (GOOS: %s, GOARCH; %s)", t, cfg.Targets[t].Os, cfg.Targets[t].Arch)
-		if e = buildKraken(tmpDir, cfg.Targets[t]); e != nil {
+		if e = buildKraken(tmpDir, cfg.Targets[t], *verbose); e != nil {
 			log.Printf("failed to build %s: %v", t, e)
 			continue
 		}
 		path := filepath.Join(*buildDir, "kraken-"+t)
+		if _, err := os.Stat(path); !os.IsNotExist(err) {
+			if *force {
+				log.Printf("force was specified, overwriting old build: %s", path)
+				os.Remove(path)
+			} else {
+				log.Printf("refusing to overwrite old build, use -force to override: %s", path)
+			}
+		}
 		if e = os.Link(filepath.Join(tmpDir, "main"), path); e != nil {
 			log.Printf("failed to link executable %s: %v", path, e)
 			continue
