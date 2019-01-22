3f05a1d01ec13685153a456b85c29a4179935a7d
J. Lowell Wofford <lowell@lanl.gov>
2019-01-22 16:40:23 -0700

    Added ability to control virtualbox nodes through the NPM vboxmanage-rest-api project

--- modules/modules.go
+++ modules/modules.go
@@ -2,7 +2,8 @@ package modules
 
 import (
 	//_ "github.com/hpc/kraken/modules/dummy"
-	_ "github.com/hpc/kraken/modules/pipower"
-	_ "github.com/hpc/kraken/modules/pipxe"
+	// _ "github.com/hpc/kraken/modules/pipower"
+	// _ "github.com/hpc/kraken/modules/pipxe"
 	_ "github.com/hpc/kraken/modules/restapi"
+	_ "github.com/hpc/kraken/modules/vboxmanage"
 )
