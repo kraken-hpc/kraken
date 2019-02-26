acfc3243eb1433a2b25ed3c694ac2cb7e03fd998
Kevin Pelzel <kpelzel0522@users.noreply.github.com>
2019-02-26 12:54:36 -0700

    updating dashboard to include services and bug fixes (#86)

--- examples/vbox/dashboard/service-worker.js
+++ examples/vbox/dashboard/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.18bd5b82e9ac01e5812e6d4623b44d9d.js"
+  "/precache-manifest.17b2f5f433e0bd641779a3bf4a3612f5.js"
 );
 
 workbox.clientsClaim();
