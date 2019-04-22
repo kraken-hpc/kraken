87bfe7a031bf953df6aba7998b550732eea31c6e
Kevin Pelzel <kevinpelzel@Kevins-MacBook-Pro.local>
2019-04-22 11:44:43 -0600

    manually updating the dashboard and removing ansible tasks to pull down dashboard from github

---
+++ examples/vbox/dashboard/precache-manifest.cc35141a556ee19b738124df64a3fb6d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9eb600ee07a27cdad64f",
+    "url": "./static/js/runtime~main.2c977d9b.js"
+  },
+  {
+    "revision": "2bd0c5018f18055ef80a",
+    "url": "./static/js/main.f016cb3c.chunk.js"
+  },
+  {
+    "revision": "9d5c15d0cec6d43b127c",
+    "url": "./static/js/2.4f1ab575.chunk.js"
+  },
+  {
+    "revision": "2bd0c5018f18055ef80a",
+    "url": "./static/css/main.87eb1c17.chunk.css"
+  },
+  {
+    "revision": "13d00e09ce85c8cc6358bb9f8459f5c1",
+    "url": "./index.html"
+  }
+];
