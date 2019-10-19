cfb2dd12820dcbf15d055418b1a323f49e6f9c7b
Kevin Pelzel <kevinpelzel22@gmail.com>
2019-10-18 18:03:04 -0600

    debuging

--- modules/websocket/websocket.go
+++ modules/websocket/websocket.go
@@ -96,7 +96,7 @@ func (w *WebSocket) Entry() {
 	c := s.Config()
 	fmt.Printf("Value: %v \n", c)
 	v, e := nself.GetValue("type.googleapis.com/proto.RestAPIConfig")
-	w.api.Logf(lib.LLDEBUG, "newvalue: %v error: %v\n", v, e)
+	fmt.Printf("newvalue: %v error: %v\n", v, e)
 	panic("something")
 	// w.api.Logf(lib.LLDEBUG, "queried for self: %+v", v)
 	// w.srvIp = IPv4.BytesToIP(v.Bytes())
