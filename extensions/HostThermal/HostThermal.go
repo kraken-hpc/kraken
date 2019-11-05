6fecb6ee61db42b9c8c88213ecd3f79f2df33949
Ghazanfar Ali <gali@celaya.local>
2019-11-05 12:08:19 -0700

    changing thermal to hostthermal

--- extensions/HostThermal/HostThermal.go
+++ extensions/HostThermal/HostThermal.go
@@ -1,4 +1,4 @@
-/* Thermal.go: extension adds enumerated states for tracking thermal conditions of node components.
+/* HostThermal.go: extension adds enumerated states for tracking thermal conditions of node components.
  *
  * Author: Ghazanfar Ali <ghazanfar.ali@ttu.edu>;Kevin Pelzel <kevinpelzel22@gmail.com>; J. Lowell Wofford <lowell@lanl.gov>
  *
@@ -7,35 +7,35 @@
  * See LICENSE file for details.
  */
 
-package thermal
+package hostthermal
 
 import (
 	"github.com/golang/protobuf/proto"
 	"github.com/golang/protobuf/ptypes"
 	"github.com/hpc/kraken/core"
-	pb "github.com/hpc/kraken/extensions/Thermal/proto"
+	pb "github.com/hpc/kraken/extensions/HostThermal/proto"
 	"github.com/hpc/kraken/lib"
 )
 
 //go:generate protoc -I ../../core/proto/include -I proto --go_out=plugins=grpc:proto proto/Thermal.proto
 
 /////////////////
-// Thermal Object /
+// HostThermal Object /
 ///////////////
 
-var _ lib.Extension = Thermal{}
+var _ lib.Extension = HostThermal{}
 
-type Thermal struct{}
+type HostThermal struct{}
 
-func (Thermal) New() proto.Message {
-	return &pb.Thermal{}
+func (HostThermal) New() proto.Message {
+	return &pb.HostThermal{}
 }
 
-func (r Thermal) Name() string {
+func (r HostThermal) Name() string {
 	a, _ := ptypes.MarshalAny(r.New())
 	return a.GetTypeUrl()
 }
 
 func init() {
-	core.Registry.RegisterExtension(Thermal{})
+	core.Registry.RegisterExtension(HostThermal{})
 }
