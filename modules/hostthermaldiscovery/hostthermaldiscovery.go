24c144fe4203c469d0853229ddbc1f86ace9ba0b
Ghazanfar Ali <gali@celaya.local>
2019-12-20 16:31:35 -0700

    bug

--- modules/hostthermaldiscovery/hostthermaldiscovery.go
+++ modules/hostthermaldiscovery/hostthermaldiscovery.go
@@ -149,11 +149,11 @@ func (hostDisc *HostDisc) Init(api lib.APIClient) {
 	hostDisc.api = api
 	hostDisc.cfg = hostDisc.NewConfig().(*pb.HostDiscoveryConfig)
 
-	hostDisc.file, err = os.OpenFile(hostDisc.cfg.GetLogHere(), os.O_WRONLY|os.O_APPEND|os.O_CREATE, 0644)
+	hostDisc.file, err := os.OpenFile(hostDisc.cfg.GetLogHere(), os.O_WRONLY|os.O_APPEND|os.O_CREATE, 0644)
 	if err != nil {
 		hostDisc.api.Logf(lib.LLERROR, "failed opening file: %v", err)
 	}
-	defer file.Close()
+	
 
 }
 
