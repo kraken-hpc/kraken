51fd1d8d5506f5fe900f76f01dc6eb74b8a247f4
Kevin Pelzel <kevinpelzel22@gmail.com>
2020-01-09 12:15:05 -0700

    updating dashboard for vbox

---
+++ examples/vbox/dashboard/precache-manifest.575552f21277b71bfa5a6fa004b18f87.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "950281950a6513d14a269550cca0e82b",
+    "url": "./index.html"
+  },
+  {
+    "revision": "aa79ee629f05f7fd3e74",
+    "url": "./static/css/main.ce8a1b51.chunk.css"
+  },
+  {
+    "revision": "759d3c83baad8e131396",
+    "url": "./static/js/2.7b3fa0db.chunk.js"
+  },
+  {
+    "revision": "2b698b292028a944173ff995c1531fab",
+    "url": "./static/js/2.7b3fa0db.chunk.js.LICENSE"
+  },
+  {
+    "revision": "aa79ee629f05f7fd3e74",
+    "url": "./static/js/main.050dec14.chunk.js"
+  },
+  {
+    "revision": "2565cb93316b76267114",
+    "url": "./static/js/runtime-main.f799fe6f.js"
+  },
+  {
+    "revision": "a986f060290bdff134e35da88bd53988",
+    "url": "./static/media/dropdown.a986f060.ico"
+  }
+]);
