145586151d660d6a521d0180d5c719cc4cf5aaf4
Kevin Pelzel <kevinpelzel22@gmail.com>
2020-08-20 14:06:31 -0600

    moving it from init to entry

--- modules/websocket/websocket.go
+++ modules/websocket/websocket.go
@@ -102,6 +102,12 @@ type Action struct {
 
 func (w *WebSocket) Entry() {
 	w.api.Logf(lib.LLDDDEBUG, "Starting entry function, %v", w.cfg)
+	nself, _ := w.api.QueryRead(w.api.Self().String())
+	srv := nself.GetService("websocket")
+	config, _ := ptypes.MarshalAny(w.cfg)
+	srv.Config = config
+	w.api.QueryUpdate(nself)
+
 	dur, _ := time.ParseDuration(w.cfg.GetTick())
 	w.api.Logf(lib.LLDEBUG, "tick: %v, duration: %v", w.cfg.GetTick(), dur)
 
@@ -224,14 +230,6 @@ func (w *WebSocket) Init(api lib.APIClient) {
 	w.mutex = &sync.Mutex{}
 	w.queue = []*Payload{}
 
-	nself, _ := w.api.QueryRead(w.api.Self().String())
-
-	srv := nself.GetService("websocket")
-
-	config, _ := ptypes.MarshalAny(w.cfg)
-	srv.Config = config
-
-	w.api.QueryUpdate(nself)
 }
 
 func (w *WebSocket) NewConfig() proto.Message {
