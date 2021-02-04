67dedda2386dad248b7b73511e5f449d92411805
J. Lowell Wofford <lowell@lanl.gov>
2021-02-04 14:32:02 -0700

    add powerapi module/extension

---
+++ extensions/powerapi/powerapi.go
@@ -0,0 +1,37 @@
+/* PowerAPI.go: extenion to use PowerAI (github.com/hpc/powerapi)
+*
+* Author: J. Lowell Wofford
+*
+* This software is open source software available under the BSD-3 license.
+* Copyright (c) 2020, Triad National Security, LLC
+* See LICENSE file for details.
+ */
+
+package powerapi
+
+import (
+	"github.com/hpc/kraken/core"
+	"github.com/hpc/kraken/lib/types"
+)
+
+//go:generate protoc -I ../../core/proto/src -I . --gogo_out=plugins=grpc:. powerapi-node.proto
+
+const Name = "type.googleapis.com/Powerman.Control"
+
+////////////////////
+// Control Object /
+//////////////////
+
+var _ types.Extension = (*Node)(nil)
+
+func (*Node) New() types.Message {
+	return &Node{}
+}
+
+func (*Node) Name() string {
+	return Name
+}
+
+func init() {
+	core.Registry.RegisterExtension(&Node{})
+}
