de51a8808b74f7c044c9bba311d8b44ae4fa57e4
J. Lowell Wofford <lowell@lanl.gov>
2021-04-18 11:43:36 -0600

    Code generation Pt. 1: app generation (#254)
    * replace kraken-build with kraken, move templates
    
    * kraken command can generate app entrypoint code
    
    * update README to reflect kraken code generation
    
    * update kitchen-sink builder and tests for new code generation
    
    * make sure stat doesn't return an error
    
    * include kraken version in generated code message

--- templates/import.go
+++ templates/import.go
@@ -1,5 +1,5 @@
 // this file is a placeholder for imports to make sure imports needed only in templates get added to go.mod
-package main
+package templates
 
 import (
 	_ "github.com/coreos/go-systemd/daemon"
