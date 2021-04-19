ce7939b3ea835c984e78d3a16236a0990d766cd5
J. Lowell Wofford <lowell@lanl.gov>
2021-04-19 15:16:53 -0600

    Code Generation, Pt. 2: Module code generation (#258)
    * add module templates
    
    * add kraken module generate functionality
    
    * add kraken module update functionality
    
    * update README with info on module generation
    
    * update app/module generation help

--- generators/global.go
+++ generators/global.go
@@ -1,4 +1,13 @@
-//go:generate go-bindata -o templates/templates.go -fs -pkg templates -prefix templates templates/app
+/* global.go: global config for generators
+ *
+ * Author: J. Lowell Wofford <lowell@lanl.gov>
+ *
+ * This software is open source software available under the BSD-3 license.
+ * Copyright (c) 2021, Triad National Security, LLC
+ * See LICENSE file for details.
+ */
+
+//go:generate go-bindata -o templates/templates.go -fs -pkg templates -prefix templates templates/app templates/module
 
 package generators
 
